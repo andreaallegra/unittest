60cde2c23638b87c5d6daa1bd5f9e122639bf2e1
aallegra <andrea.allegra@numerix.com>
2019-06-17 14:01:21 +0200

    add notes email function

--- employee.py
+++ employee.py
@@ -13,6 +13,15 @@ class Employee:
 
     @property
     def email(self):
+        """
+        to get more info on class check
+        https://www.youtube.com/watch?v=rq8cL2XMM5M&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc&index=3
+        to get more info on property decorator check
+        https://www.youtube.com/watch?v=jCzT9XFZ5bw&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc&index=6
+        also for property check
+        https://www.programiz.com/python-programming/property
+        :return:
+        """
         return '{}.{}@email.com'.format(self.first, self.last)
 
     @property
